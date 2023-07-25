0ac27cda570ed04634545092c97810b277a3d24b
TrevSh <trevonshelby@outlook.com>
2023-07-24 21:40:46 -0500

    Add project files.

---
+++ ClassAssignment22_GiveMeNumber/ClassAssignment22_GiveMeNumber.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+int askNumber(int high, int low = 1);
+
+int main()
+{
+    int number = askNumber(5);
+    std::cout << "Thanks for entering: " << number <<"\n\n";
+
+    number = askNumber(10,5);
+    std::cout << "Thanks for entering: " << number <<"\n\n";
+
+    return 0;
+}
+
+int askNumber(int high, int low) {
+    int num;
+    do {
+        std::cout << "Please enter a number" << " (" << low << " - " << high << "): ";
+        std::cin >> num;
+    } while (num > high || num < low);
+    return num;
+}
